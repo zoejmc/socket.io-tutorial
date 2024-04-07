45f27f48fec0f992f42defc10ddeb7daf1bd7e3a
zoejmc <zoejmc94@hotmail.com>
2024-04-07 10:52:12 -0400

    final comments added

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
-// Import required modules
-const express = require('express');// Importing the Express.js module for building web applications
-const path = require('path');// Importing the Path module for handling file paths
-// Create Express app
-const app = express();// Creating an instance of the Express application
-// Create HTTP server and pass the app we created
-const server = require('http').createServer(app);
-// Initialize Socket.io with server
-const io = require('socket.io')(server);// Initializing Socket.IO for real-time bidirectional event-based communication
+// In the first step, we will set up a basic Express.js server with Socket.io integration. We need to import two modules: Our Express framework and a core module used for handling file paths.
+const express = require('express');
+const path = require('path');
 
-// Store chat history
+// We will then create our express application
+const app = express();
+
+// Now we will create the HTTP server to handle incoming HTTP requests. We will pass our ‘app’ as a perimeter to the createServer function.
+const server = require('http').createServer(app);
+
+// In the next step, we will integrate Socket.io with the server instance created in the previous step. This creates a Socket.io server to work alongside our HTTP server.
+const io = require('socket.io')(server);
+
+// Next, we will create a variable to store our recent messages in an empty array. We will call it chatHistory
 const chatHistory = [];
 
-// Use static files from the 'node_modules' directory (this is for bootstrap)
+// This next line of code is for bootstrap. We ran into some issues with the styling, we figured out that we needed to use some Express middleware to serve our static style files from the node_module directory.
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-// Get the home.html file
+// Now we need to set up a route handler for the HTTP GET request to the root path of the server
 app.get('/', (req, res) => {
-    // Send the home.html file when requested
+    // Inside the callback function we need to send the home.html file located in our views folder as a response to the client's request
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
-// Tell server to start listening on port 3000
-// - When the server starts successfully, it logs a message "Server running...." to the console.
+// Now we need the server to listen for incoming connections on port 3000
 server.listen(3000, () => {
+    // We will then log a message to the console that the server is running. When the server starts running this call-back function will be executed.
     console.log("Server running....");
 });
 
-// When a client connects, set up the connection
-//set event listener for the 'connection' event
+// Now we need to listen for a ‘connection’ event emitted by Socket.io when the client connects to the server. 
 io.on('connection', (socket) => {
-    // Log a message when a user connects, along with their socket ID
-    //(everytime a user starts the server they get a new ID)
+    // We will then log a message to the console that tells the user they are connected, along with their unique ID of the socket connection.
     console.log("User connected:" + socket.id);
 
-    // Emit chat history to the new client
+    // We will then emit the ChatHistory array to our new client
     socket.emit('chatHistory', chatHistory);
 
-    // Listen for 'message' events from users
+    // Now we will set up an event listener for the ‘message’ event emitted by the client. When the client sends a message the callback function will execute.
     socket.on("message", (data) => {
-        // Add the new message to the chat history
+        // We will now add the received message data to our chatHistory array 
         chatHistory.push(data);
 
-        // Broadcast the received message to all clients
+        // Now we need to broadcast the received message data to all connected clients. 
         io.emit('message', data);
     });
 
-    // Listen for disconnection event
+    // We will now set up one final event listener. This will listen for a disconnect event emitted by socket.io when the client disconnects from the server.
     socket.on('disconnect', () => {
+        // Then we will log a message to the console saying that the user is disconnected along with their unique id
         console.log('User disconnected: ' + socket.id);
     });
 });
